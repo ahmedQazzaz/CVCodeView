17768b34f88f02a4bd7052e8229fa1118b13c69e
Ahmed Qazzaz <ahmedqazzaz@iahmed.local>
2018-05-10 14:59:54 +0300

    Initial Commit

---
+++ anbddde/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anbddde
+//
+//  Created by Ahmed Qazzaz on 5/10/18.
+//  Copyright © 2018 Ahmed Qazzaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
